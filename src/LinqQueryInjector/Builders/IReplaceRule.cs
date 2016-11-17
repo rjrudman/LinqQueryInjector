ea67c0db6a2957806993338e43e72daa3bc12951
Robert Rudman <rjrudman@gmail.com>
2016-11-17 20:32:47 +1100

    Tidy up fluent API

---
+++ src/LinqQueryInjector/Builders/IReplaceRule.cs
@@ -0,0 +1,8 @@
+﻿namespace LinqQueryInjector.Builders
+{
+	public interface IReplaceRule { }
+	public interface IReplaceRule<TEncounteredType> : IReplaceRule
+	{
+		IReplaceRule<TEncounteredType> Required(bool isRequired);
+	}
+}
